b0b5eef3ee886d3143a489e393dcbd02e957736e
Michelly Hiratsuka Zorzi <michelly.hiratsuka@gmail.com>
2020-04-21 22:35:12 -0300

    funções

--- js/main.js
+++ js/main.js
@@ -4,15 +4,16 @@ var idade2 = 55;
 var frase = "Japão é o melhor time do mundo";
 var lista = ["maçã ", " pera ", " laranja "];
 var fruta = {nome: "maçã", cor: "vermelha"};
-var frutas = [{nome: "maçã", cor: "vermelha", nome: "uva", cor: "roxa"}];
+var frutas = [{nome: "maçã", cor: "vermelha"}, {nome: "uva", cor: "roxa"}];
+var d = new Date();
 
 //alert (nome + " tem " + idade + " anos");
 alert (idade + idade2);
 console.log (nome); //o comando console permite ver o código executando no console do navegador.
 console.log (idade + idade2);
 console.log (frase.replace("Japão","Brasil")); /*.replace substitui uma palavra por outra.
-												.toLowerCase deixa as palavras em minúsculo e
-												.toUpperCase deixa as palavras em maiúsculo.*/
+									.toLowerCase deixa as palavras em minúsculo e
+									.toUpperCase deixa as palavras em maiúsculo.*/
 
 //lista.push(" uva"); - adiciona uma palavra ao final da lista.
 //lista.pop(); - retira a última palavra da lista.
@@ -28,3 +29,20 @@ alert (fruta.cor); //abre um pop-up, que imprime somente o que está dentro de "
 
 console.log (frutas); //imprime todos os elementos.
 alert (frutas[1].nome); //imprime o "nome" que está armazenado na posição 1.
+
+alert (d.getDay()); //imprime o dia atual
+alert (d.getHours()); //imprime as horas
+alert (d.getMinutes()); //imprime os minutos
+
+
+function soma (n1, n2){ //função que retorna o resultado da soma de 2 números.
+	return n1 + n2;
+}
+
+function setReplace(frase, nome novo_nome){ //função que retorna a frase escrita, com as alterações realizadas.
+	return frase.replace (nome, novo_nome);
+}
+
+alert (soma (5, 10));
+alert (setReplace ("Vai Japão", "Japão", "Brasil"));
+
